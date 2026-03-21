7ef971b9be709dc6d4d051a080375729526cbcc8
mint <15838189794@163.com>
2026-03-22 01:50:53 +0800

    sht40移植成功了喵

--- embedded/main/main.c
+++ embedded/main/main.c
@@ -5,6 +5,7 @@
 #include "Wifista.h"
 #include "Camera.h"
 #include "main.h"
+#include "sht4x.h"
 
 static const char *TAG = "main";
 static const char *secret = "b1f9562544a348c98c57a66b32a92d32";
@@ -61,8 +62,9 @@ void PhotoTransmit(camera_fb_t *fb)
     WebsocketSendbytes(fb->buf, fb->len);
 }
 
-
 void camera_transmit_task();
+
+extern void i2c_scan(void);
 
 void app_main(void)
 {
@@ -80,38 +82,78 @@ void app_main(void)
     // 创建任务执行 HTTPS 请求
     WifiSecurityClientInit();
 
-    // 通过id开启ws连接/stream/viewer/ws
-    char path_data[128];
-    snprintf(path_data, sizeof(path_data), "/api/stream/device/ws?secret=%s", secret);
-    // 注册回调函数
-    Websocket_event_handler_register(NULL, ws_text_handler);
+    // // 通过id开启ws连接/stream/viewer/ws
+    // char path_data[128];
+    // snprintf(path_data, sizeof(path_data), "/api/stream/device/ws?secret=%s", secret);
+    // // 注册回调函数
+    // Websocket_event_handler_register(NULL, ws_text_handler);
 
-    WebsocketStart("wss://lark.mintlab.top", path_data, 443);
+    // WebsocketStart("wss://lark.mintlab.top", path_data, 443);
     // WebsocketStart("ws://192.168.1.199", path_data, 8080);
 
     // 等待ws连接成功
-    while (!WebsocketIsConnected())
+    // while (!WebsocketIsConnected())
+    // {
+    //     vTaskDelay(500 / portTICK_PERIOD_MS);
+    // }
+
+    // CameraInit();
+    // sensor_t *s = esp_camera_sensor_get();
+    // s->set_framesize(s, FRAMESIZE_SVGA);
+    // s->set_vflip(s, 1);
+
+    // static uint8_t ucParameterToPass;
+    // TaskHandle_t xHandle = NULL;
+    // xTaskCreate(camera_transmit_task, "camera_transmit_task", 4096, &ucParameterToPass, 1, &xHandle);
+    // configASSERT(xHandle);
+
+    i2c_scan();
+
+    /* Initialize the i2c bus for the current platform */
+    sensirion_i2c_init();
+
+    /* Busy loop for initialization, because the main loop does not work without
+     * a sensor.
+     */
+    while (sht4x_probe() != STATUS_OK)
     {
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        printf("SHT sensor probing failed\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+    printf("SHT sensor probing successful\n");
 
-    CameraInit();
-    sensor_t *s = esp_camera_sensor_get();
-    s->set_framesize(s, FRAMESIZE_SVGA);
-    s->set_vflip(s, 1);
-
-    static uint8_t ucParameterToPass;
-    TaskHandle_t xHandle = NULL;
-    xTaskCreate(camera_transmit_task, "camera_transmit_task", 4096, &ucParameterToPass, 1, &xHandle);
-    configASSERT(xHandle);
-}
-
-void camera_transmit_task(){
     while (1)
     {
-        if (device.status == DEVICE_ON_STREAM){
+        int32_t temperature, humidity;
+        /* Measure temperature and relative humidity and store into variables
+         * temperature, humidity (each output multiplied by 1000).
+         */
+        int8_t ret = sht4x_measure_blocking_read(&temperature, &humidity);
+        if (ret == STATUS_OK)
+        {
+            printf("measured temperature: %0.2f degreeCelsius, "
+                   "measured humidity: %0.2f percentRH\n",
+                   temperature / 1000.0f, humidity / 1000.0f);
+        }
+        else
+        {
+            printf("error reading measurement\n");
+        }
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); /* sleep 1s */
+    }
+}
+
+void camera_transmit_task()
+{
+    while (1)
+    {
+        if (device.status == DEVICE_ON_STREAM)
+        {
             CameraTakePhoto(PhotoTransmit);
-        }else{
+        }
+        else
+        {
             ESP_LOGI("camera_transmit_task", "待机模式");
             vTaskDelay(3000 / portTICK_PERIOD_MS);
         }
